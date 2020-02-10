32544a7868a5cbf485b237e30ab337ec2952495c
Mohamed <mohammed.korany.ali@gmail.com>
2020-02-10 15:06:35 +0200

    Initial Commit

---
+++ VictoryLinkTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VictoryLinkTask
+//
+//  Created by Mohamed Korany Ali on 2/10/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
